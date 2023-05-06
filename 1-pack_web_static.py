88de68bd024d808fc7b8cb3245b8bf15f7bc77c1
ShogoMark <trigmark87@gmail.com>
2023-05-06 09:00:45 +0000

    task1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#script that generates a .tgz archive from the contents of the web_static folder
+
+import os
+from datetime import datetime
+from fabric.api import local
+
+def do_pack():
+    dt = datetime.now()
+    filename = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                             dt.month,
+                                                             dt.day,
+                                                             dt.hour,
+                                                             dt.minute,
+                                                             dt.second)
+    
+    if os.path.isdir("./versions") is False:
+        if local('mkdir versions').failed is True:
+            return None
+    if local("tar -cvf {} web_static".format(filename)).failed is True:
+        return None
+    return filename
