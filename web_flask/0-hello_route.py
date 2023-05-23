a996db90eb679cb78a8aeac8faf8c7b73835d964
ShogoMark <trigmark87@gmail.com>
2023-05-23 14:14:55 +0000

    init

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,10 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-"""function that returns Hello HBNB"""
-
-
 def hello():
+    """function that returns Hello HBNB"""
     return("Hello HBNB!")
 
 
-if "__name__" == " __main__":
+if __name__ == " __main__":
     app.run(host='0.0.0.0', port=5000)
